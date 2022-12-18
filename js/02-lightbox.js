3d2c53b1e91cc786ecc056ba0476e4ac5d626da4
Krystian Guz <krystian.guz@outlook.com>
2022-12-18 17:41:14 +0100

    Task 2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+//Tworzenie i renderowanie znacznika zgodnie z tablicą danych galleryItems
+//i dostarczonym szablonem elementu galerii.
+let markup = "";
+for (const { preview, original, description } of galleryItems) {
+  markup += `
+    <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" >
+    </a>
+  `;
+}
+gallery.innerHTML = markup;
+
+//Inicjalizacja biblioteki po utworzeniu elementów galerii
+//i dodaniu ich do div.gallery
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
